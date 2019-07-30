e5f8936a05ba5367039492935b026b1daa166c7d
shockham <samuel.hockham@gmail.com>
2019-07-30 20:40:18 +0100

    Update actix-web to 1.0 and fix fallout

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
 extern crate actix_web;
-use actix_web::{http, server, App, HttpRequest, Path, Responder};
+use actix_web::{HttpServer, App, HttpRequest, web, Responder};
 
 fn index(_req: HttpRequest) -> impl Responder {
     "Hello Person! id:?"
 }
 
-fn hello(info: Path<(u32, String)>) -> impl Responder {
+fn hello(info: web::Path<(u32, String)>) -> impl Responder {
     format!("Hello {}! id:{}", info.1, info.0)
 }
 
-fn main() {
-    server::new(|| {
+fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
         App::new()
-            .route("/", http::Method::GET, index)
-            .route("/{id}/{name}/", http::Method::GET, hello)
-    }).bind("0.0.0.0:80")
+            .route("/", web::get().to(index))
+            .route("/{id}/{name}/", web::get().to(hello))
+    }).bind("0.0.0.0:8080")
         .unwrap()
-        .run();
+        .run()
 }
